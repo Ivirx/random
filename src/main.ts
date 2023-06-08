6ac20eaa00d22be70e8c4a8141c6b957f875a8ae
Vivek Kumar <scorpianva@gmail.com>
2023-06-08 10:49:12 +0530

    Now it is Done.

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,14 @@ const number = document.querySelector("#number") as HTMLHeadingElement;
 const start = document.querySelector("#start") as HTMLInputElement;
 const end = document.querySelector("#end") as HTMLInputElement;
 const button = document.querySelector("#generate-button") as HTMLButtonElement;
+const form = document.querySelector("#inputs") as HTMLFormElement;
+
 
 let intervalId = 0;
 
-button.onclick = () => {
+const generateRandomNumber = (event: (SubmitEvent | null)) => {
+    event?.preventDefault();
+
     let startingValue = Number(start.value);
     let endingValue = Number(end.value);
 
@@ -52,5 +56,19 @@ button.onclick = () => {
         if (displayedNumber === randomNumber)
             clearInterval(intervalId);  // Stop the interval when the displayed number is equal to the random number
 
-    }, 20);
+    }, 22);
+}
+
+button.onclick = () => generateRandomNumber(null);
+form.onsubmit = (e) => generateRandomNumber(e);
+
+
+window.onload = () => {
+    number.innerText = "0";
+    start.value = "0";
+    end.value = "99";
+
+    setTimeout(() => {
+        generateRandomNumber(null);
+    }, 470);
 }
